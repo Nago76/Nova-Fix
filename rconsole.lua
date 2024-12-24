0d9c2b9f101ec4be39a4b19923b5ec9433363fce
ItsDev <132821601+Nago76@users.noreply.github.com>
2024-12-24 12:08:23 +0800

    rc7 reel!!!

---
+++ rconsole.lua
@@ -0,0 +1,39 @@
+-- Probably faked ig
+-- if u skid u gay
+-- author | Dev | Merdeka Developer
+
+rconsoleclear = function()
+
+end
+
+rconsolecreate = function()
+
+end
+
+rconsoledestroy = function()
+
+end
+
+rconsoleinput = function()
+
+end
+
+rconsoleprint = function(...)
+	print(...)
+end
+
+rconsolesettitle = function(title)
+	if (title == "") then
+		title = "pro"
+	else	
+
+	end
+end
+
+consoleclear = rconsoleclear
+consolecreate = rconsolecreate
+consoledestroy = rconsoledestroy
+consoleinput = rconsoleinput
+consoleprint = rconsoleprint
+rconsolename = rconsolesettitle
+consoletitle = rconsolesettitle
